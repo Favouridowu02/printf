1cea26ef472160635a84fa439958a1c4cca40710
Favour Idowu <favouridowu02@gmail.com>
2023-09-17 13:42:20 -0600

    done

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Prints a character
+ * @c: the character to be passed
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - Write a function that produces output according to a format
+ * @format: a character string. The format string is composed of zero or
+ * more directives
+ * Return: stringlength
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
+	int j;
+	char *s;
+
+	va_start(ap, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i += 1;
+			switch (format[i])
+			{
+			case 'c':
+				_putchar(va_arg(ap, int));
+				break;
+			case 's':
+				j = 0;
+				s = va_arg(ap, char *);
+
+				while (s[j] != '\0')
+				{
+					_putchar(s[j]);
+					j++;
+				}
+				break;
+			case '%':
+				_putchar('%');
+				break;
+			default:
+				_putchar(format[i]);
+				break;
+			}
+		}
+	}
+	i += j;
+	_putchar('\n');
+	va_end(ap);
+	return (i);
+}
+
