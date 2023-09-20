0b30585718d9187c9cb74d3762531c957ac23b65
Favour Idowu <favouridowu02@gmail.com>
2023-09-20 17:14:40 -0600

    done

--- main.h
+++ main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <limits.h>
 
+int pswitch(va_list ap, const char *format, int i);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int pswitch(va_list ap, const char *format, int i);
 int print_decimal(int);
 #endif
