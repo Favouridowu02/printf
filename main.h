699b4fed6cef3ee054aebeb99a0b22ef71cc020f
Favour Idowu <favouridowu02@gmail.com>
2023-09-20 17:11:48 -0600

    done

--- main.h
+++ main.h
@@ -6,6 +6,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int  pswitch(va_list ap, const char *format, int i);
+int pswitch(va_list ap, const char *format, int i);
 int print_decimal(int);
 #endif
